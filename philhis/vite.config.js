2cffb79d55fd052963a0c681f7ad563d880f2dc0
Elijah Crisehea Nollen <emnollen@student.apc.edu.ph>
2026-03-23 14:34:30 +0000

    Removed tailwind.css

--- philhis/vite.config.js
+++ philhis/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/philhis-luisa-camagay/philhis/'
+  plugins: [react()],
+  base: '/philhis-luisa-camagay/philhis/',
 })
